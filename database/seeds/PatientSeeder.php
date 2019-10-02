9720410ee049e46c82920d882a672d1d8d303620
MACE <lucas.mace@etu.univ-lyon1.fr>
2019-10-02 20:24:40 +0200

    des vrais noms car c'est mieux pour le système de recherche

--- database/seeds/PatientSeeder.php
+++ database/seeds/PatientSeeder.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
 
 class PatientSeeder extends Seeder
 {
@@ -13,10 +12,12 @@ class PatientSeeder extends Seeder
      */
     public function run()
     {
+        $nom = ["Christina","Ceola","Janita","Jackelyn","Carri","Russel","Fumiko","Madeline","Susannah","Percy","Isadora","Salley","Suellen","Rosaura","Kina","Harvey","Magdalena","Hyo","Sherley","Dorris","Ginny","Chris","Eldon","Adele","Evonne","Stephan","Ryan","Elmer","Lloyd","Jeromy","Bunny","Zena","Malisa","Chere","Linette","Deena","Teodora","Tanja","Doug","Kaitlin","Nova","September","Arlinda","Deb","Dania","Wan","Zaida","Arvilla","Tama","Rosaria",];
+        $prenom = ["Clevenger","Chynoweth","Janco","Jobe","Connell","Rene","Floyd","Marcantel","Suarez","Paugh","Iwamoto","Santo","Schoonmaker","Raulerson","Klumpp","Hirata","Mcgary","Huffine","Stoney","Dearman","Gangestad","Clonts","Engelking","Armbruster","Evangelista","Sant","Radcliff","Erazo","Leonardi","July","Bankston","Zoller","Mccallister","Cotner","Linzy","Deslauriers","Takemoto","Tomaselli","Dinges","Kershner","Neel","Shepherd","Akridge","Difilippo","Dittman","Wardle","Zabel","Angus","Towery","Royston",];
         for($i = 0; $i < 50; $i++){
             DB::table('patients')->insert([
-                'nom' => Str::random(),
-                'prenom' => Str::random(),
+                'nom' => $nom[$i],
+                'prenom' => $prenom[$i],
             ]);
         }
     }
