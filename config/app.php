2dce240b76e62c22e6867822dfdec883086d8b95
MACE <lucas.mace@etu.univ-lyon1.fr>
2019-11-01 11:45:47 +0100

    ajout de scout et de tntsearch

--- config/app.php
+++ config/app.php
@@ -166,6 +166,9 @@ return [
          * Package Service Providers...
          */
 
+        TeamTNT\Scout\TNTSearchScoutServiceProvider::class,
+        Laravel\Scout\ScoutServiceProvider::class,
+
         /*
          * Application Service Providers...
          */
