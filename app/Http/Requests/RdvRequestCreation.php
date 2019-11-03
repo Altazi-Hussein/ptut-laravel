ecc9384c0acc6eba556928556602dc99af624ac7
p1806978 <ali.hussein@etu.univ-lyon1.fr>
2019-11-04 00:12:17 +0100

    Fix: le champ de date est maintenant obligatoire + message d'erreur si non renseigné (remarque: il sera à delete plus tard car ce sera à l'algo de gérer la date du RDV)

--- app/Http/Requests/RdvRequestCreation.php
+++ app/Http/Requests/RdvRequestCreation.php
@@ -26,7 +26,8 @@ class RdvRequestCreation extends FormRequest
         return [
             'raison' => 'required',
             'lastName' => 'required',
-            'firstName' => 'required'
+            'firstName' => 'required',
+            'started_at' => 'required'
         ];
     }
 }
